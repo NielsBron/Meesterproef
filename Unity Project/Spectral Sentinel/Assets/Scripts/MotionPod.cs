2951f3a13d118f53e4927e1b6746fc3188bb59fc
nielsbron <nnbbron@gmail.com>
2024-05-23 01:57:37 +0200

    Motion Pod Changes
    -Added Beeping sound (placeholder)
    -Changes pitch depending on how close u are.

--- Unity Project/Spectral Sentinel/Assets/Scripts/MotionPod.cs
+++ Unity Project/Spectral Sentinel/Assets/Scripts/MotionPod.cs
@@ -4,126 +4,158 @@ using UnityEngine;
 
 public class MotionPod : MonoBehaviour
 {
-  [Header("Materials")]
-  [SerializeField] public Material RedMat;
-  [SerializeField] public Material GreenMat;
-  [SerializeField] public Material YellowMat;
-  [SerializeField] public Material BlueMat;
-  [SerializeField] public Material PurpleMat;
-  [SerializeField] public Material WhiteMat;
+    [Header("Materials")]
+    [SerializeField] public Material RedMat;
+    [SerializeField] public Material GreenMat;
+    [SerializeField] public Material YellowMat;
+    [SerializeField] public Material BlueMat;
+    [SerializeField] public Material PurpleMat;
+    [SerializeField] public Material WhiteMat;
 
-  [Header("Lights")]
-  [SerializeField] public GameObject RedObject;
-  [SerializeField] public GameObject GreenObject;
-  [SerializeField] public GameObject YellowObject;
-  [SerializeField] public GameObject BlueObject;
-  [SerializeField] public GameObject PurpleObject;
+    [Header("Lights")]
+    [SerializeField] public GameObject RedObject;
+    [SerializeField] public GameObject GreenObject;
+    [SerializeField] public GameObject YellowObject;
+    [SerializeField] public GameObject BlueObject;
+    [SerializeField] public GameObject PurpleObject;
 
-  private GameObject RedLight;
-  private GameObject GreenLight;
-  private GameObject YellowLight;
-  private GameObject BlueLight;
-  private GameObject PurpleLight;
-  
-  private bool isOn = false;
-  private GameObject playerObj = null;
+    [Header("Distance parameters")]
+    [SerializeField] public float GreenDistance = default;
+    [SerializeField] public float YellowDistance = default;
+    [SerializeField] public float BlueDistance = default;
+    [SerializeField] public float PurpleDistance = default;
+
+    [Header("Audio Clip")]
+    [SerializeField] public AudioClip clip;
+    [Header("Pitch Settings")]
+    [SerializeField] public float pitchGreen = 1.0f;
+    [SerializeField] public float pitchYellow = 1.1f;
+    [SerializeField] public float pitchBlue = 1.2f;
+    [SerializeField] public float pitchPurple = 1.3f;
+
+    private GameObject RedLight;
+    private GameObject GreenLight;
+    private GameObject YellowLight;
+    private GameObject BlueLight;
+    private GameObject PurpleLight;
     
-  void Start()
-  {
-    RedLight = RedObject.transform.GetChild(0).gameObject;
-    GreenLight = GreenObject.transform.GetChild(0).gameObject;
-    YellowLight = YellowObject.transform.GetChild(0).gameObject;
-    BlueLight = BlueObject.transform.GetChild(0).gameObject;
-    PurpleLight = PurpleObject.transform.GetChild(0).gameObject;
+    private bool isOn = false;
+    private GameObject playerObj = null;
 
-    playerObj = GameObject.FindGameObjectWithTag("Player");
-    TurnOn();
-  }
+    private AudioSource audioSource;
 
-  void Update()
-  {
-    if (playerObj != null && isOn == true)
+    void Start()
     {
-      float distance = Vector3.Distance(transform.position, playerObj.transform.position);
-      // Debug.Log("Distance to player: " + distance);
+        RedLight = RedObject.transform.GetChild(0).gameObject;
+        GreenLight = GreenObject.transform.GetChild(0).gameObject;
+        YellowLight = YellowObject.transform.GetChild(0).gameObject;
+        BlueLight = BlueObject.transform.GetChild(0).gameObject;
+        PurpleLight = PurpleObject.transform.GetChild(0).gameObject;
 
-      if (distance <= 0.4f)
-      {
-        Purple();
-      }
-      else if (distance <= 0.6f)
-      {
-        Blue();
-      }
-      else if (distance <= 0.8f)
-      {
-        Yellow();
-      }
-      else if (distance <= 1.0f)
-      {
-        Green();
-      }
-      else
-      {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        audioSource = GetComponent<AudioSource>();
+
         TurnOn();
-      }
     }
-  }
 
-  public void TurnOn()
-  {
-    isOn = true;
-    DisableAll();
-    RedObject.GetComponent<MeshRenderer> ().material = RedMat;
-    RedLight.SetActive(true);
-  }
+    void Update()
+    {
+        if (playerObj != null && isOn == true)
+        {
+            float distance = Vector3.Distance(transform.position, playerObj.transform.position);
+            Debug.Log("Distance to player: " + distance);
 
-  public void Green()
-  {
-    DisableAll();
-    GreenObject.GetComponent<MeshRenderer> ().material = GreenMat;
-    GreenLight.SetActive(true);
-  }
+            if (distance <= PurpleDistance)
+            {
+                Purple();
+            }
+            else if (distance <= BlueDistance)
+            {
+                Blue();
+            }
+            else if (distance <= YellowDistance)
+            {
+                Yellow();
+            }
+            else if (distance <= GreenDistance)
+            {
+                Green();
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
+    }
 
-  public void Yellow()
-  {
-    DisableAll();
-    YellowObject.GetComponent<MeshRenderer> ().material = YellowMat;
-    YellowLight.SetActive(true);
-  }
+    public void TurnOn()
+    {
+        isOn = true;
+        DisableAll();
+        RedObject.GetComponent<MeshRenderer>().material = RedMat;
+        RedLight.SetActive(true);
+        audioSource.Stop(); // Stop playing audio when turning on red light
+    }
 
-  public void Blue()
-  {
-    DisableAll();
-    BlueObject.GetComponent<MeshRenderer> ().material = BlueMat;
-    BlueLight.SetActive(true);
-  }
+    public void Green()
+    {
+        DisableAll();
+        GreenObject.GetComponent<MeshRenderer>().material = GreenMat;
+        GreenLight.SetActive(true);
+        audioSource.clip = clip;
+        audioSource.pitch = pitchGreen;
+        audioSource.Play();
+    }
 
-  public void Purple()
-  {
-    DisableAll();
-    PurpleObject.GetComponent<MeshRenderer> ().material = PurpleMat;
-    PurpleLight.SetActive(true);
-  }
+    public void Yellow()
+    {
+        DisableAll();
+        YellowObject.GetComponent<MeshRenderer>().material = YellowMat;
+        YellowLight.SetActive(true);
+        audioSource.clip = clip;
+        audioSource.pitch = pitchYellow;
+        audioSource.Play();
+    }
 
-  public void TurnOff()
-  {
-    isOn = false;
-    DisableAll();
-  }
+    public void Blue()
+    {
+        DisableAll();
+        BlueObject.GetComponent<MeshRenderer>().material = BlueMat;
+        BlueLight.SetActive(true);
+        audioSource.clip = clip;
+        audioSource.pitch = pitchBlue;
+        audioSource.Play();
+    }
 
-  public void DisableAll()
-  {
-    RedObject.GetComponent<MeshRenderer> ().material = WhiteMat;
-    GreenObject.GetComponent<MeshRenderer> ().material = WhiteMat;
-    YellowObject.GetComponent<MeshRenderer> ().material = WhiteMat;
-    BlueObject.GetComponent<MeshRenderer> ().material = WhiteMat;
-    PurpleObject.GetComponent<MeshRenderer> ().material = WhiteMat;
+    public void Purple()
+    {
+        DisableAll();
+        PurpleObject.GetComponent<MeshRenderer>().material = PurpleMat;
+        PurpleLight.SetActive(true);
+        audioSource.clip = clip;
+        audioSource.pitch = pitchPurple;
+        audioSource.Play();
+    }
 
-    RedLight.SetActive(false);
-    GreenLight.SetActive(false);
-    YellowLight.SetActive(false);
-    BlueLight.SetActive(false);
-    PurpleLight.SetActive(false);
-  }
+    public void TurnOff()
+    {
+        isOn = false;
+        DisableAll();
+        audioSource.Stop();
+    }
+
+    public void DisableAll()
+    {
+        RedObject.GetComponent<MeshRenderer>().material = WhiteMat;
+        GreenObject.GetComponent<MeshRenderer>().material = WhiteMat;
+        YellowObject.GetComponent<MeshRenderer>().material = WhiteMat;
+        BlueObject.GetComponent<MeshRenderer>().material = WhiteMat;
+        PurpleObject.GetComponent<MeshRenderer>().material = WhiteMat;
+
+        RedLight.SetActive(false);
+        GreenLight.SetActive(false);
+        YellowLight.SetActive(false);
+        BlueLight.SetActive(false);
+        PurpleLight.SetActive(false);
+    }
 }
